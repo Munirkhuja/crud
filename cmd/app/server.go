90316e0d67762d971a8a00cb021135b11884f4bf
munirhuja1357@gmail.com <bit314151>
2022-01-04 03:42:37 +0500

    first commit

---
+++ cmd/app/server.go
@@ -0,0 +1,239 @@
+package app
+
+import (
+	"encoding/json"
+	"errors"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/Munirkhuja/crud/pkg/customers"
+)
+
+type Server struct {
+	mux *http.ServeMux
+	customersSvc *customers.Service
+}
+func NewServer(mux *http.ServeMux,customersSvc *customers.Service)*Server  {
+	return &Server{mux: mux,customersSvc: customersSvc}
+}
+func (s *Server)ServeHTTP(writer http.ResponseWriter,request *http.Request)  {
+	s.mux.ServeHTTP(writer,request)
+}
+func (s *Server)Init()  {
+	s.mux.HandleFunc("/customers.getById",s.handleGetCustomersByID)
+	s.mux.HandleFunc("/customers.getAll",s.handleGetAll)
+	s.mux.HandleFunc("/customers.getAllActive",s.handleGetAllActive)
+	s.mux.HandleFunc("/customers.save",s.handleSave)
+	s.mux.HandleFunc("/customers.removeById",s.handleRemoveByID)
+	s.mux.HandleFunc("/customers.blockById",s.handleBlockByID)
+	s.mux.HandleFunc("/customers.unblockById",s.handleUnblockByID)
+}
+func (s *Server)handleGetCustomersByID(writer http.ResponseWriter,request *http.Request)  {
+	idParam:=request.URL.Query().Get("id")
+	id,err:=strconv.ParseInt(idParam,10,64)
+	if err!=nil {
+		log.Print(err)
+		http.Error(writer,http.StatusText(http.StatusBadRequest),http.StatusBadRequest)
+		return
+	}
+	item,err:=s.customersSvc.ByID(request.Context(),id)
+	if errors.Is(err,customers.ErrNotFound) {
+		http.Error(writer,http.StatusText(http.StatusNotFound),http.StatusNotFound)
+		return
+	}
+	if err!=nil {
+		http.Error(writer,http.StatusText(http.StatusBadRequest),http.StatusBadRequest)
+		return		
+	}
+	
+	data,derr:=json.Marshal(item)
+	if derr!=nil {
+		log.Print(derr)
+		http.Error(writer,http.StatusText(http.StatusInternalServerError),http.StatusInternalServerError)
+		return				
+	}
+	writer.Header().Set("Content-Type","aplication/json")
+	_,err=writer.Write(data)
+	if err!=nil {
+		log.Print(err)
+	}
+}
+
+func (s *Server)handleGetAll(writer http.ResponseWriter,request *http.Request)  {	
+	items,err:=s.customersSvc.All(request.Context())
+	if errors.Is(err,customers.ErrNotFound) {
+		http.Error(writer,http.StatusText(http.StatusNotFound),http.StatusNotFound)
+		return
+	}
+	if err!=nil {
+		http.Error(writer,http.StatusText(http.StatusBadRequest),http.StatusBadRequest)
+		return		
+	}
+	
+	data,derr:=json.Marshal(items)
+	if derr!=nil {
+		log.Print(derr)
+		http.Error(writer,http.StatusText(http.StatusInternalServerError),http.StatusInternalServerError)
+		return				
+	}
+	writer.Header().Set("Content-Type","aplication/json")
+	_,err=writer.Write(data)
+	if err!=nil {
+		log.Print(err)
+	}
+}
+func (s *Server)handleGetAllActive(writer http.ResponseWriter,request *http.Request)  {	
+	items,err:=s.customersSvc.AllActive(request.Context())
+	if errors.Is(err,customers.ErrNotFound) {
+		http.Error(writer,http.StatusText(http.StatusNotFound),http.StatusNotFound)
+		return
+	}
+	if err!=nil {
+		http.Error(writer,http.StatusText(http.StatusBadRequest),http.StatusBadRequest)
+		return		
+	}
+	
+	data,derr:=json.Marshal(items)
+	if derr!=nil {
+		log.Print(derr)
+		http.Error(writer,http.StatusText(http.StatusInternalServerError),http.StatusInternalServerError)
+		return				
+	}
+	writer.Header().Set("Content-Type","aplication/json")
+	_,err=writer.Write(data)
+	if err!=nil {
+		log.Print(err)
+	}
+}
+
+func (s *Server)handleSave(writer http.ResponseWriter,request *http.Request)  {
+	parseerr:=request.ParseMultipartForm(10*1024*1024)
+	if parseerr!=nil {
+		log.Print(parseerr)
+		http.Error(writer,http.StatusText(http.StatusInternalServerError),http.StatusInternalServerError)
+		return
+	}
+	postForm:=request.PostForm
+	name,phone:="",""
+
+	if nameParam,exists:=postForm["name"];exists {
+		name=nameParam[0]
+	}
+	if phoneParam,exists:=postForm["phone"];exists {
+		phone=phoneParam[0]
+	}
+	item,err:=s.customersSvc.Save(request.Context(),name,phone)
+	if errors.Is(err,customers.ErrNotFound) {
+		http.Error(writer,http.StatusText(http.StatusNotFound),http.StatusNotFound)
+		return
+	}
+	if err!=nil {
+		http.Error(writer,http.StatusText(http.StatusBadRequest),http.StatusBadRequest)
+		return		
+	}
+	
+	data,derr:=json.Marshal(item)
+	if derr!=nil {
+		log.Print(derr)
+		http.Error(writer,http.StatusText(http.StatusInternalServerError),http.StatusInternalServerError)
+		return				
+	}
+	writer.Header().Set("Content-Type","aplication/json")
+	_,err=writer.Write(data)
+	if err!=nil {
+		log.Print(err)
+	}
+}
+
+func (s *Server)handleRemoveByID(writer http.ResponseWriter,request *http.Request)  {
+	idParam:=request.URL.Query().Get("id")
+	id,err:=strconv.ParseInt(idParam,10,64)
+	if err!=nil {
+		log.Print(err)
+		http.Error(writer,http.StatusText(http.StatusBadRequest),http.StatusBadRequest)
+		return
+	}
+	err=s.customersSvc.RemoveByID(request.Context(),id)
+	if errors.Is(err,customers.ErrNotFound) {
+		http.Error(writer,http.StatusText(http.StatusNotFound),http.StatusNotFound)
+		return
+	}
+	if err!=nil {
+		http.Error(writer,http.StatusText(http.StatusBadRequest),http.StatusBadRequest)
+		return		
+	}
+	
+	data,derr:=json.Marshal(id)
+	if derr!=nil {
+		log.Print(derr)
+		http.Error(writer,http.StatusText(http.StatusInternalServerError),http.StatusInternalServerError)
+		return				
+	}
+	writer.Header().Set("Content-Type","aplication/json")
+	_,err=writer.Write(data)
+	if err!=nil {
+		log.Print(err)
+	}
+}
+
+func (s *Server)handleBlockByID(writer http.ResponseWriter,request *http.Request)  {
+	idParam:=request.URL.Query().Get("id")
+	id,err:=strconv.ParseInt(idParam,10,64)
+	if err!=nil {
+		log.Print(err)
+		http.Error(writer,http.StatusText(http.StatusBadRequest),http.StatusBadRequest)
+		return
+	}
+	item,err:=s.customersSvc.BlockByID(request.Context(),id)
+	if errors.Is(err,customers.ErrNotFound) {
+		http.Error(writer,http.StatusText(http.StatusNotFound),http.StatusNotFound)
+		return
+	}
+	if err!=nil {
+		http.Error(writer,http.StatusText(http.StatusBadRequest),http.StatusBadRequest)
+		return		
+	}
+	
+	data,derr:=json.Marshal(item)
+	if derr!=nil {
+		log.Print(derr)
+		http.Error(writer,http.StatusText(http.StatusInternalServerError),http.StatusInternalServerError)
+		return				
+	}
+	writer.Header().Set("Content-Type","aplication/json")
+	_,err=writer.Write(data)
+	if err!=nil {
+		log.Print(err)
+	}
+}
+func (s *Server)handleUnblockByID(writer http.ResponseWriter,request *http.Request)  {
+	idParam:=request.URL.Query().Get("id")
+	id,err:=strconv.ParseInt(idParam,10,64)
+	if err!=nil {
+		log.Print(err)
+		http.Error(writer,http.StatusText(http.StatusBadRequest),http.StatusBadRequest)
+		return
+	}
+	item,err:=s.customersSvc.UnblockByID(request.Context(),id)
+	if errors.Is(err,customers.ErrNotFound) {
+		http.Error(writer,http.StatusText(http.StatusNotFound),http.StatusNotFound)
+		return
+	}
+	if err!=nil {
+		http.Error(writer,http.StatusText(http.StatusBadRequest),http.StatusBadRequest)
+		return		
+	}
+	
+	data,derr:=json.Marshal(item)
+	if derr!=nil {
+		log.Print(derr)
+		http.Error(writer,http.StatusText(http.StatusInternalServerError),http.StatusInternalServerError)
+		return				
+	}
+	writer.Header().Set("Content-Type","aplication/json")
+	_,err=writer.Write(data)
+	if err!=nil {
+		log.Print(err)
+	}
+}
---
+++ pkg/customers/service.go
@@ -0,0 +1,151 @@
+package customers
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"log"
+	"time"
+)
+
+var ErrNotFound = errors.New("item not found")
+var ErrInternal = errors.New("internal error")
+type Service struct{
+	db *sql.DB
+}
+func NewService(db *sql.DB)*Service  {
+	return &Service{db: db}
+}
+type Customer struct{
+	ID int64 `json:"id"`
+	Name string `json:"name"`
+	Phone string `json:"phone"`
+	Active string `json:"active"`
+	Created time.Time `json:"created"`
+}
+func (s *Service)ByID(ctx context.Context,id int64)(*Customer,error) {
+	item:=&Customer{}
+	err:=s.db.QueryRowContext(ctx,`
+	select id,name,phone,active,created from customers where id=$1
+	`,id).Scan(&item.ID,&item.Name,&item.Phone,&item.Active,&item.Created)
+	if errors.Is(err,sql.ErrNoRows) {
+		return nil,ErrNotFound
+	}
+	if err!=nil {
+		log.Print(err)
+		return nil,ErrInternal
+	}
+	return item,nil
+}
+func (s *Service)All(ctx context.Context)([]*Customer,error) {
+	items:=make([]*Customer,0)
+	rows,err:=s.db.QueryContext(ctx,`
+	select id,name,phone,active,created from customers`)
+	if err!=nil {
+		log.Print(err)
+		return nil,ErrInternal
+	}
+	defer func() {
+		if cerr:=rows.Close();err!=nil {
+			log.Print(cerr)
+		}
+	}()
+	for rows.Next(){
+		item:=&Customer{}
+		err:=rows.Scan(&item.ID,&item.Name,&item.Phone,&item.Active,&item.Created)
+		if err!=nil {
+			log.Print(err)
+			return nil,err
+		}
+		items = append(items, item)
+	}
+	err=rows.Err()
+	if err!=nil {
+		log.Print(err)
+		return nil,err
+	}
+	return items,nil
+}
+func (s *Service)AllActive(ctx context.Context)([]*Customer,error) {
+	items:=make([]*Customer,0)
+	rows,err:=s.db.QueryContext(ctx,`
+	select id,name,phone,active,created from customers where active`)
+	if err!=nil {
+		log.Print(err)
+		return nil,ErrInternal
+	}
+	defer func() {
+		if cerr:=rows.Close();err!=nil {
+			log.Print(cerr)
+		}
+	}()
+	for rows.Next(){
+		item:=&Customer{}
+		err:=rows.Scan(&item.ID,&item.Name,&item.Phone,&item.Active,&item.Created)
+		if err!=nil {
+			log.Print(err)
+			return nil,err
+		}
+		items = append(items, item)
+	}
+	err=rows.Err()
+	if err!=nil {
+		log.Print(err)
+		return nil,err
+	}
+	return items,nil
+}
+func (s *Service)Save(ctx context.Context,name string,phone string)(*Customer,error) {
+	item:=&Customer{}
+	err:=s.db.QueryRowContext(ctx,`
+	insert into customers(name,phone) values($1,$2) 
+	on conflict(phone) do update set name=excluded.name 
+	RETURNING id,name,phone,active,created;
+	`,name,phone).Scan(&item.ID,&item.Name,&item.Phone,&item.Active,&item.Created)
+	if err!=nil {
+		log.Print(err)
+		return nil,ErrInternal
+	}
+	return item,nil
+}
+func (s *Service)RemoveByID(ctx context.Context,id int64)(error) {
+	_,err:=s.db.ExecContext(ctx,`
+	delete from customers where id=$1
+	`,id)
+	if err!=nil {
+		log.Print(err)
+		return ErrInternal
+	}
+	return nil
+}
+func (s *Service)BlockByID(ctx context.Context,id int64)(*Customer,error) {
+	item:=&Customer{}
+	err:=s.db.QueryRowContext(ctx,`
+	update customers set active=false where id=$1	
+	RETURNING id,name,phone,active,created
+	`,id).Scan(&item.ID,&item.Name,&item.Phone,&item.Active,&item.Created)
+	if errors.Is(err,sql.ErrNoRows) {
+		return nil,ErrNotFound
+	}
+	if err!=nil {
+		log.Print(err)
+		return nil,ErrInternal
+	}
+	return item,nil
+}
+
+func (s *Service)UnblockByID(ctx context.Context,id int64)(*Customer,error) {
+	item:=&Customer{}
+	err:=s.db.QueryRowContext(ctx,`
+	update customers set active=true where id=$1	
+	RETURNING id,name,phone,active,created
+	`,id).Scan(&item.ID,&item.Name,&item.Phone,&item.Active,&item.Created)
+	if errors.Is(err,sql.ErrNoRows) {
+		return nil,ErrNotFound
+	}
+	if err!=nil {
+		log.Print(err)
+		return nil,ErrInternal
+	}
+	return item,nil
+}
